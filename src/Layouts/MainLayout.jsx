2bb3f4f8b8a4e90212e5b39d8cf9d45c9d6e6a8e
SaniaAhmed1 <tasminsania77@gmail.com>
2023-05-01 08:49:40 -0700

    basic setup done

---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Header from '../components/Header';
+import { Outlet } from 'react-router-dom';
+import Footer from '../components/Footer';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default MainLayout;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDzBB7nIyL--rgi_3ENTH0-zi0yEvgZyVo",
+  authDomain: "the-rannaghor.firebaseapp.com",
+  projectId: "the-rannaghor",
+  storageBucket: "the-rannaghor.appspot.com",
+  messagingSenderId: "949260471362",
+  appId: "1:949260471362:web:31f722ad057deb2d1971db"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app 
